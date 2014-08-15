2d8634642e981d6743440c7157dea57225d53292
LeeAnn Kinney <leeann.a.kinney@gmail.com>
2014-08-15 11:06:03 -0400

    trying to integrate with google maps

--- js/controllers.js
+++ js/controllers.js
@@ -10,3 +10,21 @@ myApp.controller("MyCtrl2" ,function ($scope) {
 });
 
 // you may add more controllers below
+// var controller = module.controller('MapCtrl', function($scope) {
+//   $scope.myMarkers = [
+//       {
+//          "latitude":33.22,
+//          "longitude":35.33
+//       },
+//       ...
+//   ];
+
+//   $scope.center = {
+//       latitude: 33.895497,
+//       longitude: 35.480347,
+//   };
+
+//   $scope.zoom = 13;
+//   $scope.markers = $scope.myMarkers;
+//   $scope.fit = true;
+// });
